271955f1963ad50576e99f7bc87853b6842431a9
kristoffer-kopp <89389386+kristoffer-kopp@users.noreply.github.com>
2024-01-29 20:23:45 +0100

    Changed Lockmode on PlayerLook
    Added Lock

--- RitualPuzzleGame/Assets/Scripts/Player/PlayerLook.cs
+++ RitualPuzzleGame/Assets/Scripts/Player/PlayerLook.cs
@@ -16,8 +16,7 @@ public class PlayerLook: MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false; 
+        LockState();
     }
 
     // Update is called once per frame
@@ -30,15 +29,28 @@ public class PlayerLook: MonoBehaviour
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -85f, 85f); //limits rotation upwards and downwords
-
-        playerCamera.rotation = Quaternion.Euler(xRotation, yRotation, 0); //rotates camera , used for up and downrotation
-        transform.rotation = Quaternion.Euler(0, yRotation, 0); // rotates player, used for left and right rotation
-
+        if (Input.GetKey(KeyCode.LeftControl) && Cursor.visible!=true){ UnLockState(); }
+        if (!Input.GetKey(KeyCode.LeftControl))
+        {
+            if(Cursor.visible != false){ LockState(); }
+            playerCamera.rotation = Quaternion.Euler(xRotation, yRotation, 0); //rotates camera , used for up and downrotation
+            transform.rotation = Quaternion.Euler(0, yRotation, 0); // rotates player, used for left and right rotation
+        }
     }
 
     private void OnLook(InputValue value) //gets mouse input 
     {
         mouseX = value.Get<Vector2>().x;
         mouseY = value.Get<Vector2>().y;
+    }
+    private void LockState()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    private void UnLockState()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
