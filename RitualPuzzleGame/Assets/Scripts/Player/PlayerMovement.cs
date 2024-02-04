19a47c0d8cb1295341e24cbefd43ecd01ef0172d
NielsEriksson <89368371+NielsEriksson@users.noreply.github.com>
2024-02-04 13:30:15 +0100

    player jump + hitboxes on jumpable items

--- RitualPuzzleGame/Assets/Scripts/Player/PlayerMovement.cs
+++ RitualPuzzleGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,35 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.Contracts;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController controller;
-    private Vector3 movement;
+
+    private Vector3 movementInput;
     [SerializeField] private float moveSpeed;
+    [SerializeField]private Vector3 movementVector;
+
+    private float gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 3.0f;
+    private float verticalVelocity;
+
+    [SerializeField] private float jumpPower;
+  
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        controller.detectCollisions = true;
     }
 
     void FixedUpdate()
     {
-        if (controller.isGrounded == false)
-        {
-            controller.Move(new Vector3(0, -1, 0)); //used to add gravity when the player is not grounded 
-        }
-        Vector3 horizontalVelocity = transform.right * movement.x + transform.forward * movement.z;
-        if (movement != Vector3.zero)
-        {
-            controller.Move(horizontalVelocity * Time.fixedDeltaTime * moveSpeed);
-        }
 
+        ApplyMovement();
+        ApplyGravity();
+
+    }
+    private void ApplyMovement ()
+    {
+        if (movementInput != Vector3.zero)
+        {
+            movementVector = transform.right * movementInput.x + transform.forward * movementInput.z;
+        }
+        movementVector = new Vector3(movementVector.x, verticalVelocity, movementVector.z);
+        controller.Move(movementVector * Time.fixedDeltaTime * moveSpeed);
+        movementVector = new Vector3(0, movementVector.y, 0);
+    }
+    private void ApplyGravity()
+    {
+        movementVector.y = verticalVelocity;
+        if (IsGrounded() && verticalVelocity <0.0f)
+        {
+            verticalVelocity = -1f;
+        }
+        else
+        {
+          verticalVelocity += ( gravity * gravityMultiplier * Time.deltaTime);
+        }
+        
     }
     private void OnMove(InputValue value) //gets wasd input and transoforms it to a 3d vector
     {
-        movement = new Vector3(value.Get<Vector2>().x, 0, value.Get<Vector2>().y);
+        movementInput = new Vector3(value.Get<Vector2>().x, 0, value.Get<Vector2>().y);
+        
+    }
+    private void OnJump(InputValue value)
+    {
+        if (!IsGrounded())
+        {
+            return;
+        }
+        Debug.Log("Jumping");
+        verticalVelocity += jumpPower;
+    }
+    public bool IsGrounded()
+    {
+        return Physics.Raycast(controller.transform.position + Vector3.up * 0.1f, Vector3.down, 0.75f);
     }
 }
