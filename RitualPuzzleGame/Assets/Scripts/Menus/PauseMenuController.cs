581766de1176fbdf3e70e5b992ee903cb92ca657
BenDeLaBod <benjamin.bodin02@gmail.com>
2024-02-13 16:13:42 +0100

    end sensor

--- RitualPuzzleGame/Assets/Scripts/Menus/PauseMenuController.cs
+++ RitualPuzzleGame/Assets/Scripts/Menus/PauseMenuController.cs
@@ -8,14 +8,36 @@ public class PauseMenuController : MonoBehaviour
 
     [SerializeField] private GameObject playing, paused, handIcon;
 
+    [SerializeField] private GameObject inventoryBar;
+
+    [SerializeField] private PlayerLook playerLook;
+
     public void OnToggleMenu()
     {
         isPaused = !isPaused;
         
         playing.SetActive(!isPaused);
         paused.SetActive(isPaused);
-        if(isPaused ) Cursor.lockState = CursorLockMode.None;
-        else Cursor.lockState = CursorLockMode.Locked;
+        if(isPaused ) Pause();
+        else Unpause();
+    }
+
+    public void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        handIcon.SetActive(false);
+        inventoryBar.SetActive(false);
+        playerLook.enabled = false;
+    }
+
+    public void Unpause()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        handIcon.SetActive(true);
+        inventoryBar.SetActive(true);
+        playerLook.enabled = true;
     }
 
     public void Quit()
